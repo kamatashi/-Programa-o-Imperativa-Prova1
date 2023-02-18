90c041bea3769b62f151d03d2390d91bc4894bf6
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2023-02-17 22:41:42 -0300

    fix: correct calculation
    Não li a questão corretamente e cometi um grande erro. A própria questão já dava o cálculo com o exemplo.

--- questao3.py
+++ questao3.py
@@ -1,19 +1,16 @@
+# A minha lógica estava errada, não me atentei para a formula apresentada. Está é a solução para o problema:
+
 salario = float(input())
 
-# Na prova, fiz cada calculo direto nos IFs. Aqui deixei separado para não repetir o código
-faixaSal1 = salario * 0.075
-faixaSal2 = salario * 0.09
-faixaSal3 = salario * 0.12
-faixaSal4 = salario * 0.14
 
-# Adicionei <=, pois os valores pertencem a faixa salarial indicada
-
-# Na prova, cometi um erro grave.
+# Adicionei <=, pois os valores pertencem a faixa salarial indicada.
+# É feito o cálculo das faixas salariais e descontados do salário
+# Incluído a formatação correta do número
 if salario <= 1302:
-    print(salario - (faixaSal1))
+    print('%.2f'%(salario - ((salario * 0.075))))
 elif salario <= 2571.29:
-    print(salario - (faixaSal1 + faixaSal2))
+    print('%.2f'%(salario - ((1302 * 0.075) + ((salario - 1302) * 0.09))))
 elif salario <= 3856.94:
-    print(salario - (faixaSal1 + faixaSal2 + faixaSal3))
+    print('%.2f'%(salario - ((1302 * 0.075) + ((2571.29 - 1302) * 0.09) + ((salario - 2571.29) * 0.12))))
 else:
-    print(salario - (faixaSal1 + faixaSal2 + faixaSal3 + faixaSal4))
+    print('%.2f'%(salario - ((1302 * 0.075) + ((2571.29 - 1302) * 0.09) + ((3856.94 - 2571.29) * 0.12) + (salario  - 3856.94)* 0.14)))
